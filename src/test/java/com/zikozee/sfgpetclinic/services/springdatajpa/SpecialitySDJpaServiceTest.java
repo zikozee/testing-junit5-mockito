a3b2ab5fcc9792518c4c7fa3d2aac1d898212280
ezekiel.eromosei <ezekiel.eromosei@longbridgetech.com>
2021-05-08 23:55:33 +0100

    throwing exceptions in mockito

--- src/test/java/com/zikozee/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
+++ src/test/java/com/zikozee/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
@@ -13,8 +13,7 @@ import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.BDDMockito.then;
+import static org.mockito.BDDMockito.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -112,6 +111,33 @@ class SpecialitySDJpaServiceTest {
         service.delete(new Speciality());
 
         //then
+        then(specialtyRepository).should().delete(any());
+    }
+
+    @Test
+    void testDoThrow() {
+        doThrow(new RuntimeException("boom")).when(specialtyRepository).delete(any());
+
+        assertThrows(RuntimeException.class, () ->specialtyRepository.delete(new Speciality()));
+
+        verify(specialtyRepository).delete(any());
+    }
+
+    @Test
+    void testFindByIdThrows() {
+        given(specialtyRepository.findById(1L)).willThrow(new RuntimeException("boom"));
+
+        assertThrows(RuntimeException.class, () -> service.findById(1L));
+
+        then(specialtyRepository).should().findById(1L);
+    }
+
+    @Test
+    void testDeleteBDD() {
+        willThrow(new RuntimeException("boom")).given(specialtyRepository).delete(any());
+
+        assertThrows(RuntimeException.class, ()-> specialtyRepository.delete(new Speciality()));
+
         then(specialtyRepository).should().delete(any());
     }
 }
